5ae30b9c1dba123e14f3ce177aaaf0c98569ffe4
Benjamin DiGregorio <bdigrego@calpoly.edu>
2015-01-26 01:46:23 -0800

    Added daily weather forecast for Cal Poly campus

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     window.setInterval(getTime, 1000);
+    getTemp();
 };
 
 function getTime() {
@@ -8,7 +9,7 @@ function getTime() {
     var minutes = leadingZero(date.getMinutes());
     var seconds = leadingZero(date.getSeconds());
     var time = hours + ":" + minutes + ":" + seconds;
-    
+
     document.getElementById('clock').innerHTML = time;
 }
 
@@ -17,4 +18,41 @@ function leadingZero(timeVal) {
         return "0" + timeVal;
     }
     return timeVal;
+}
+
+function getTemp() {
+    var url = 'https://api.forecast.io/forecast/45a099af090167125dac1eb898fe443d/35.300399,-120.662362?callback=?';
+    var success = function(data) {
+        var icon = 'img/' + data.daily.icon + '.png';
+        var forecast = $('<p class="small">').html(data.daily.summary);
+        var bgColor = getBgColor(data.daily.data[0].temperatureMax);
+
+        $('#forecast-icon').attr('src', icon);
+        $('#forecast-label').html(forecast);
+        $('body').addClass(bgColor);
+    };
+
+    $.getJSON(url, success);
+
+    function getBgColor(temp) {
+        var color;
+
+        if (temp < 60) {
+            color = 'cold';
+        }
+        else if (temp < 70) {
+            color = 'chilly';
+        }
+        else if (temp < 80) {
+            color = 'nice';
+        }
+        else if (temp < 90) {
+            color = 'warm';
+        }
+        else {
+            color = 'hot';
+        }
+
+        return color;
+    }
 }
